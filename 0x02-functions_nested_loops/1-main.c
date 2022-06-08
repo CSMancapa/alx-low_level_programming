097b48f63c9d0e8639e947ede3b8848a00c3f437
CSMancapa <cmancapa@gmail.com>
2022-06-08 08:04:41 -0700

    done

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,12 +1,12 @@
-#include <unistd.h>
 #include "main.h"
+
 /**
- * main - Entry point
+ * main - check the code for Holberton high school students
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(1, '_putchar\n');
+	print_alphabet();
 	return (0);
 }
