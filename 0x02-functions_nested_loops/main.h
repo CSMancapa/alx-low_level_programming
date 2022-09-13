583de14fa66abee54e5de3b198af50e1b15427df
Cebo Mancapa <cmancapa@gmail.com>
2022-09-13 10:39:11 -0700

    Main header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,28 +1,18 @@
-#ifndef _ MAIN_H_
-#define _ MAIN_H_
+#ifndef MAIN_H_
+#define MAIN_H_
 
-/*
- * main - Prints _putchar, followed by a new line
- *
- * Returns: Always 0
- */
-int main(void)
-{
-	int _putchar(char c);
-	void print_alphabet(void);
-	void print_alphabet_x10(void);
-	int _islower(int c);
-	int _isalpha(int c);
-	int print_sign(int n);
-	int _abs(int);
-	int print_last_digit(int);
-	void jack_bauer(void);
-	void times_table(void);
-	int add(int, int);
-	void print_to_98(int n);
-	void print_times_table(int n);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
-	return (0);
-}
-
-#endif /*_MAIN_H_*/
+#endif
