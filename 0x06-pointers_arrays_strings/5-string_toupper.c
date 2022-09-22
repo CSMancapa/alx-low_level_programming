0928e1479f1d7fe03a087bc05470db85f4da1b8e
Cebo Mancapa <cmancapa@gmail.com>
2022-09-22 07:30:25 -0700

    function that changes all lowercase letters of a string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,29 +8,21 @@
  * Return: char pointer
  */
 
-
 char *string_toupper(char *str)
 {
-		char l;
+	char l;
 
-		int i = 0;
+	int i = 0;
 
-		while (str[i] != '\0')
-		{
+	while (str[i] != '\0')
+	{
 		for (l = 'a'; l <= 'z'; l++)
 		{
 			if (str[i] == l)
 			{
-																				int num = ((int)str[i]) - 32;
-
-																						str[i] = (char)num;
-
-																						break;
-																					}
-																				}
-
-		i++;
+				i++;
+			}
 		}
-
-		return (str);
+	}
+	return (str);
 }
