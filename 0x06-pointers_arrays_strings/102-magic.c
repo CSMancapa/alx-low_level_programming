07bca0f88b71736ae322723d5562c49aa9c83f67
CSMancapa <cmancapa@gmail.com>
2022-10-11 04:50:25 -0700

    final push

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -10,15 +10,14 @@ int main(void)
 	p = &n;
 	/*
 	 * write your line of code here...
-	 * Remember:
-	 * - not allowed to use a
-	 * - not allowed to modify the variable p
-	 * - can only write one statement
-	 * - not allowed to use ,
-	 * - not allowed to code anything else than the line of expected line of code
+	 * Remember
+	 * - you are not allowed to use a
+         * - you are not allowed to modify p
+         * - only one statement
+         * - you are not allowed to code anything else than this line of code
 	 */
-		*(p + 5) = 98;
+	p[5] = 98;
 	/* ...so that this prints 98\n */
-	sprintf("a[2] = %d\n", a[2]);
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
